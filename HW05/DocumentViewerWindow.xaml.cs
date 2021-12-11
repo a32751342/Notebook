8f7a469f09b6ef8850b77e9ebc67470a0c8b3496
柏瑜 曾 <zxc1112508@gmail.com>
2021-12-11 17:25:19 +0800

    加入專案檔案。

---
+++ HW05/DocumentViewerWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HW05
+{
+    /// <summary>
+    /// DocumentViewerWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class DocumentViewerWindow : Window
+    {
+        public DocumentViewerWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 36, 48, 72 };
+        }
+
+        /*打開文件*/
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //MessageBox.Show("File open");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Rich Text Format|*.rtf|All Files|*.*";
+            if(openFileDialog.ShowDialog()==true)
+            {
+                FileStream fs = new FileStream(openFileDialog.FileName,FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fs, DataFormats.Rtf);
+            }
+        }
+
+        /*儲存文件*/
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Rich Text Format|*.rtf|All Files|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        /*變換字型*/
+        private void cmbFontFamily_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)//變換字型
+        {
+            if (cmbFontFamily.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, cmbFontFamily.SelectedItem);
+            }
+        }
+
+        /*變更字體大小*/
+        private void cmbFontSize_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (cmbFontSize.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, cmbFontSize.SelectedItem);
+            }
+        }
+    }
+}
---
+++ HW05/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW05
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentViewerWindow documentViewerWindow = new DocumentViewerWindow();//產生新object
+            documentViewerWindow.Show();//產生新視窗(可以不用結束新視窗就可以再新增一個)
+        }
+    }
+}
